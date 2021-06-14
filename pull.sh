d0a546374d06bfe7cb44d3526df8296c147e61ef
srcrs <srcrs@foxmail.com>
2021-06-14 15:07:45 +0800

    fix bug pull

--- pull.sh
+++ pull.sh
@@ -3,6 +3,9 @@
 cd /root
 #复制出用户的信息
 cp ./UnicomTask-docker/config.json ./
+#复制脚本
+cp ./UnicomTask-docker/start.sh ./
+cp ./UnicomTask-docker/pull.sh ./
 #删除原代码
 rm -rf ./UnicomTask-docker/
 #拉取最新代码
@@ -11,5 +14,10 @@ git clone https://github.com/srcrs/UnicomTask-docker.git
 rm ./UnicomTask-docker/config.json
 #将用户信息再复制到该目录
 cp ./config.json ./UnicomTask-docker/
+#复制脚本
+cp ./start.sh ./UnicomTask-docker/
+cp ./pull.sh ./UnicomTask-docker/
 #删除多余
 rm ./config.json
+rm ./start.sh
+rm ./pull.sh
