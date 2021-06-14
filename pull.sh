d1a150b8e8eb412f5316d13e67dc19c57c1b0c71
srcrs <srcrs@foxmail.com>
2021-06-14 14:42:25 +0800

    add pull.sh auto update

---
+++ pull.sh
@@ -0,0 +1,13 @@
+# !/usr/bin/bash
+
+cd root
+#复制出用户的信息
+cp ./UnicomTask-docker/config.json ./
+#删除原代码
+rm -rf ./UnicomTask-docker/
+#拉取最新代码
+git clone https://github.com/srcrs/UnicomTask-docker.git
+#将用户信息再复制到该目录
+cp ./config.json ./UnicomTask-docker/
+#删除多余
+rm ./config.json
